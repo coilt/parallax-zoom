740f47cb6f383e1769109f270a8aafe09d4e2ab2
coilt <cameraboi@gmail.com>
2024-02-12 15:19:24 +0700

    Add lenis smooth scroll

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,24 @@
+'use client'
 import styles from './page.module.css'
 import ZoomParallax from '../components/ZoomParallax'
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis()
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
   return (
     <main className={styles.main}>
+      <div className={styles.spacer} />
+
       <ZoomParallax />
       {console.log('ZoomParallax is loaded')}
     </main>
