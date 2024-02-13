c6bee89d3238c161e3e1f00f9dc36149b22f8c75
coilt <cameraboi@gmail.com>
2024-02-13 13:52:19 +0700

    Fix page width

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,21 @@
-'use client';
-import styles from './page.module.css';
-import ZoomParallax from '../components/ZoomParallax';
-import ContinueGallery from '../components/ContinueGallery';
-import Lenis from '@studio-freight/lenis';
-import { useEffect } from 'react';
-import Image from 'next/image';
-import { motion, useScroll, useTransform } from 'framer-motion';
-import TextAnimateIn from './components/animations/text/hero/textanimatein.jsx';
+'use client'
+import styles from './page.module.css'
+import ZoomParallax from '../components/ZoomParallax'
+import ContinueGallery from '../components/ContinueGallery'
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from 'react'
+import Image from 'next/image'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import TextAnimateIn from './components/animations/text/hero/textanimatein.jsx'
 
-const introText = 'Photographer and cinematograper.'
+const introText = 'Photographer and cinematographer.'
 
 export default function Home() {
   const { scrollYProgress } = useScroll()
-  // Adjust the range to reduce the movement, making the image scroll slower  const y = useTransform(scrollYProgress, [0, 1], [0, -200]); // Adjust range as needed
   const heroY = useTransform(scrollYProgress, [0, 1], [0, 1000])
-  const spacerY = useTransform(scrollYProgress, [0, 1], [800, -8000])
-  const textY = useTransform(scrollYProgress, [0, 1], [1000, -3000]) // Text moves differently
-  const textOpacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
-
+  const textY = useTransform(scrollYProgress, [0, 1], [1000, -3000])
+  const textOpacity = useTransform(scrollYProgress, [0, 1], [0, 1])
+  const backplateY = useTransform(scrollYProgress, [0, 1], [1000, -10000])
 
   useEffect(() => {
     const lenis = new Lenis()
@@ -25,67 +23,46 @@ export default function Home() {
       lenis.raf(time)
       requestAnimationFrame(raf)
     }
-
     requestAnimationFrame(raf)
   }, [])
 
   return (
-     
-      
-      <main className={styles.main}>
-
- 
-
-        <motion.div className={styles.heroTitle} style={{ y: textY }}>
-          Katerina Ernst
-        </motion.div>
-
-<div className={styles.heroLead}>
-        {/* Apply both y position and opacity to TextAnimateIn via style prop */}
+    <main className={styles.main}>
+      <motion.div className={styles.heroTitle} style={{ y: textY }}>
+        Katerina Ernst
+      </motion.div>
+      <div className={styles.heroLead}>
         <TextAnimateIn value={introText} style={{ opacity: textOpacity }} />
-       
-        </div>
-            
-        <motion.div className={styles.heroLead} style={{ y: textY }}>
-
-
-              <p> <span className={styles.paragraphLead}>
-              I enjoy helping people discover something in them, they didn't
-              know they had. Through my hands, through my eyes, through my lens.
-              I bring to life the unknowable. The unseeable.
-            </span>
-          </p>
+      </div>
+      <motion.div className={styles.heroLead} style={{ y: textY }}>
+        <p>
+          {' '}
+          <span className={styles.paragraphLead}>
+            I enjoy helping people discover something in them, they didn't know
+            they had. Through my hands, through my eyes, through my lens. I
+            bring to life the unknowable. The unseeable.
+          </span>
+        </p>
+      </motion.div>
+      <div className={styles.heroimage}>
+        <motion.div
+          className={styles.imageOverlayContainer}
+          style={{ y: heroY }}
+        >
+          <Image
+            src={'/images/hero_cr.jpg'}
+            alt='hero image'
+            fill
+          />
         </motion.div>
-
-        <div className={styles.heroimage}>
-          <motion.div
-            className={styles.imageOverlayContainer}
-            style={{ y: heroY }}
-          >
-            <Image
-              src={'/images/hero_cc.jpg'}
-              width={1920}
-              height={1080}
-               
-              alt='hero image'
-            />
-          </motion.div>
-        </div>
-
-        <div className={styles.spacercontainer}>
-          <motion.div style={{ y: spacerY }} className={styles.spacer}>
-            {' '}
-          </motion.div>
-        </div>
-        <div>
-          <ZoomParallax />
-        </div>
-
-        <div className={styles.gallerycontinue}>
-          <h1>Another gallery</h1>
-          <ContinueGallery />
-        </div>
-      </main>
-     
+      </div>
+      <div className={styles.gallery}>
+        <ZoomParallax />
+      </div>
+      <div className={styles.gallerycontinue}>
+        <h1>Another gallery</h1>
+        <ContinueGallery />
+      </div>
+    </main>
   )
 }
