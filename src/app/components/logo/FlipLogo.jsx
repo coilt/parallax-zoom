5371106017b2547916cddbc7b1550a6afed3dacc
coilt <cameraboi@gmail.com>
2024-02-18 18:42:34 +0700

    Add working logo to compact logo fade animation

--- src/app/components/logo/FlipLogo.jsx
+++ src/app/components/logo/FlipLogo.jsx
@@ -1,49 +1,53 @@
-// FlipLogo.jsx
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import FullLogo from '../svgs/logo-full.svg';  // Adjust the path as necessary
-import CompactLogo from '../svgs/logo-compact.svg';
+import FullLogo from '../svgs/logo-full.svg';  
+import CompactLogo from '../svgs/logo-compact.svg';  
 import styles from './FlipLogo.module.scss';  
 import Image from 'next/image';
 
 const FlipLogo = () => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  // Variants for flipping animation
-  const flipVariants = {
-    front: { rotateY: 0 },
-    back: { rotateY: 180 },
-  };
-
-  // Toggle flip state
-  const toggleFlip = () => {
-    setIsFlipped(!isFlipped);
-  };
+  const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      // Example condition: flip when scrolling more than 100px down
-      setIsFlipped(window.scrollY > 100);
+      setIsScrolled(window.scrollY > 10);
     };
-  
+
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  // Ensuring transitions are properly applied for each state
+  const logoVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  // Define transitions with specific properties for entering and exiting
+  const enterTransition = { duration: 0.5, delay: 0.3 };
+  const exitTransition = { duration: 0.5 };
+
   return (
-    <motion.div 
-  className={styles.logoContainer}
-  variants={flipVariants}
-  animate={isFlipped ? 'back' : 'front'}
-  transition={{ duration: 0.5 }}
-  style={{ perspective: 1000 }}
->
-  {isFlipped ? (
-    <Image src={CompactLogo} alt="Compact Logo" layout="fill" objectFit="contain" />
-  ) : (
-    <Image src={FullLogo} alt="Full Logo" layout="fill" objectFit="contain" />
-  )}
-</motion.div>
+    <div className={styles.logoContainer}>
+      <motion.div
+        className={styles.logo}
+        variants={logoVariants}
+        initial="hidden"
+        animate={!isScrolled ? "visible" : "hidden"}
+        transition={!isScrolled ? enterTransition : exitTransition}
+      >
+        <Image src={FullLogo} alt="Full Logo" layout="fill" objectFit="contain" />
+      </motion.div>
+      <motion.div
+        className={styles.logo}
+        variants={logoVariants}
+        initial="hidden"
+        animate={isScrolled ? "visible" : "hidden"}
+        transition={isScrolled ? enterTransition : exitTransition}
+      >
+        <Image src={CompactLogo} alt="Compact Logo" layout="fill" objectFit="contain" />
+      </motion.div>
+    </div>
   );
 };
 
